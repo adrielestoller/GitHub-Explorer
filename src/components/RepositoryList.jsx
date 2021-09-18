dffc57ff5ec63c5e29c0a2fb3c568d918a095e82
Adriel Estoller <adrielestoller@nave.org.br>
2021-09-18 13:37:08 -0300

    Propriedades de Componentes

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,18 +1,21 @@
-const repositoryName = 'unform2';
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: "https://github.com/unform/unform"
+}
 
 export function RepositoryList (){
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
+            
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem />
+                <RepositoryItem />
+                <RepositoryItem />
             </ul>
         </section>
     );
