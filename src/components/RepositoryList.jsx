9ff0a190d91c11eb76c48772bee9f6089c6177ad
Adriel Estoller <adrielestoller@nave.org.br>
2021-09-18 15:06:10 -0300

    Chamadas HTTPS

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,22 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import './../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: "https://github.com/unform/unform"
-}
-
 export function RepositoryList (){
+    const [repositories, setRespositories] = useState([]);
+    useEffect(() =>{
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRespositories(data));
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem />
-                <RepositoryItem />
-                <RepositoryItem />
+                { repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />) }
             </ul>
         </section>
     );
