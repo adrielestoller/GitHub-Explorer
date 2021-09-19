6ab28e986b8a44fa2c89d21df1daac8b97392a53
Adriel Estoller <adrielestoller@nave.org.br>
2021-09-19 01:35:57 -0300

    Implementação TypeScript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import './../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList (){
-    const [repositories, setRespositories] = useState([]);
+    const [repositories, setRespositories] = useState<Repository[]>([]);
     useEffect(() =>{
         fetch('https://api.github.com/orgs/rocketseat/repos')
             .then(response => response.json())
